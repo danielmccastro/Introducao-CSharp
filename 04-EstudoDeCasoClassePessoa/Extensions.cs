35190c9d655240dc5bf0e482525612544384965b
Daniel Castro <danielmccastro@gmail.com>
2025-06-13 10:46:53 -0300

    Estudo de Caso Classe Pessoa

---
+++ 04-EstudoDeCasoClassePessoa/Extensions.cs
@@ -0,0 +1,15 @@
+static class Extensions
+{
+    public static bool HasOnlyDigits(this string valor)
+    {
+        const string digitos = "0123456789";
+        foreach (char c in valor)
+        {
+            if (!digitos.Contains(c))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ 04-EstudoDeCasoClassePessoa/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("Albert", "Einstein", new DateTime(1955, 6, 17), "12345678900");
+        p1.Peso = 76;
+        p1.Altura = 1.78;
+        p1.MostrarDados();
+
+        p1.Comer(2.5);
+        p1.Comer(3800);
+        p1.Correr(7, 30);
+        p1.MostrarDados();
+
+        Console.WriteLine("");
+
+        Pessoa p2 = new Pessoa("Ada", "Lovelace", new DateTime(1974, 4, 18), "12345678900", 62, 1.65);
+        p2.MostrarDados();
+
+        p2.Comer(2.5);
+        p2.Comer(3800);
+        p2.Correr(7, 30);
+        p2.MostrarDados();
+    }
+}
